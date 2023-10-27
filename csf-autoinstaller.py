982bfd57b7678bde0e0a750d57863a755c307ee5
rasulsh <me@rasulsh.ir>
2023-10-27 19:51:29 +0330

    update project to python

--- csf-autoinstaller.py
+++ csf-autoinstaller.py
@@ -1,162 +1,155 @@
-#!/bin/bash
-rpm -qa | grep dialog;
-if [ $? -ne 0 ]; then
-	yum install dialog -y ;
-fi
-##Global varibales
-CSF="/usr/sbin/csf";
-CDF="/etc/csf/csf.deny";
-CONFIG="/etc/csf/csf.conf";
-IGNORE="/etc/csf/csf.pignore";
-TO="yourmail\@domain.com";
-FROM="hostname\@domain.com";
-MYSSH="";
-RESET='\033[0m'
-GREEN='\033[01;32m'
-INPUT=/tmp/menu.sh.$$
-OUTPUT=/tmp/output.sh.$$
+import os
+import re
+
+regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+
+def checkmail(email):
+    if(re.fullmatch(regex, email)):
+        return True
+    return False
+
+def find_package_manager():
+    if os.path.exists('/usr/bin/yum'):
+        return 'yum'
+    elif os.path.exists('/usr/bin/dnf'):
+        return 'dnf'
+    elif os.path.exists('/usr/bin/apt'):
+        return 'apt'
+    else:
+        return False
+
+def package_installer(package_name):
+    package_manager = find_package_manager()
+    if not package_manager:
+        print('Package manager not found')
+        exit(1)
+    if isinstance(package_name, list):
+        for package in package_name:
+            os.system('{} install -y {}'.format(package_manager, package))
+    else:
+        os.system('{} install -y {}'.format(package_manager, package_name))
+
+def update_all_package(pkmanager):
+    if pkmanager == 'yum':
+        os.system('yum update -y')
+    elif pkmanager == 'dnf':
+        os.system('dnf update -y')
+    elif pkmanager == 'apt':
+        os.system('apt update -y')
+    else:
+        print('Package manager not found')
+        exit(1)
+
+def change_current_dir(new_dir):
+    os.chdir(new_dir)
+
+def run_command(command):
+    os.system(command)
+
+def get_bash_path():
+    return os.environ['SHELL']
+
+def replace_port_vars(file_content,var_name,append_values):
+    for i, line in enumerate(file_content):
+        if line.startswith(var_name):
+            key, value = line.strip().split('=')
+            ports = value.strip().strip('"').split(',')
+            ports += append_values.split(',')
+            updated_value = '"' + ','.join(ports) + '"'
+            file_content[i] = f'{key} = {updated_value}\n'
+            break
+    return file_content
+
+def configure_csf():
+    # csf config file : /etc/csf/csf.conf
+    with open('/etc/csf/csf.conf', 'r+') as f:
+        # start config
+        lines = f.readlines()
+        lines[lines.index('TESTING = "1"\n')] = 'TESTING = "0"\n'
+        lines[lines.index('RESTRICT_SYSLOG = "0"\n')] = 'RESTRICT_SYSLOG = "3"\n'
+        lines[lines.index('DENY_TEMP_IP_LIMIT = "100"\n')] = 'DENY_TEMP_IP_LIMIT = "900"\n'
+        lines[lines.index('LF_IPSET = "0"\n')] = 'LF_IPSET = "1"\n'
+        lines[lines.index('LF_SELECT = "0"\n')] = 'LF_SELECT = "1"\n'
+        lines[lines.index('CT_LIMIT = "0"\n')] = 'CT_LIMIT = "800"\n'
+        lines[lines.index('PT_USERMEM = "512"\n')] = 'PT_USERMEM = "0"\n'
+        lines[lines.index('PT_LIMIT = "60"\n')] = 'PT_LIMIT = "0"\n'
+        lines[lines.index('PT_SKIP_HTTP = "0"\n')] = 'PT_SKIP_HTTP = "1"\n'
+        lines[lines.index('LF_EMAIL_ALERT = "1"\n')] = 'LF_EMAIL_ALERT = "0"\n'
+        lines[lines.index('LF_PERMBLOCK_ALERT = "1"\n')] = 'LF_PERMBLOCK_ALERT = "0"\n'
+
+        
+        read_administrator_email = input('Enter administrator email: ')
+        
+        if read_administrator_email == '' or checkmail(read_administrator_email) == False:
+            print('Administrator email is empty or invalid, skip')
+        else:
+            lines[lines.index('LF_ALERT_TO = ""\n')] = 'LF_ALERT_TO = "{}"\n'.format(read_administrator_email)
+        
+        read_custom_TCP_ports    = input('Enter custom TCP PORTS (seperate with english comma): ')
+
+        if read_custom_TCP_ports == '':
+            print('Custom TCP ports is empty, skip')
+        else:
+            lines = replace_port_vars(lines,'TCP_IN',read_custom_TCP_ports)
+            lines = replace_port_vars(lines,'TCP_OUT',read_custom_TCP_ports)
+
+        read_custom_UDP_ports    = input('Enter custom UDP PORTS (seperate with english comma): ')
+
+        if read_custom_UDP_ports == '':
+            print('Custom UDP ports is empty, skip')
+        else:
+            lines = replace_port_vars(lines,'UDP_IN',read_custom_UDP_ports)
+            lines = replace_port_vars(lines,'UDP_OUT',read_custom_UDP_ports)
+
+        
+        # write config
+        f.seek(0)
+        f.writelines(lines)
+        f.truncate()
+        
+        # end config
+        f.close()
+
+## Main
+update_all_package(find_package_manager())
+
+package_installer('tar wget nano')
+
+change_current_dir('/tmp')
+
+run_command('mkdir csfinstall')
+
+change_current_dir('/tmp/csfinstall')
+
+run_command('wget "https://download.configserver.com/csf.tgz"')
+
+run_command('tar -xzf csf.tgz')
+
+change_current_dir('/tmp/csfinstall/csf')
+
+run_command('{} install.sh'.format(get_bash_path()))
+
+package_installer([
+    'perl-libwww-perl',
+    'perl-LWP-Protocol-https',
+    'perl-GDGraph',
+    'liblwp-protocol-https-perl',
+    'libgd-graph-perl',
+    'perl-Crypt-SSLeay',
+    'perl-Time-HiRes',
+    'perl-Math-BigInt',
+    'perl-Math-BigInt-GMP',
+    'perl-Math-BigInt-FastCalc',
+    'ipset'
+])
+
+configure_csf()
+
+run_command('csf -x ; csf -e ; clear')
+run_command('systemctl status lfd')
+print('\n----------------------------------------------\n')
+run_command('systemctl status lfd')
 
 
-if [ -d "/usr/local/cpanel" ]; then
-	NPO_IN='TCP_IN = \"20,21,22,25,53,80,110,143,443,465,587,993,995,2789,27635,1443,9085,9086,2077,2078,2082,2083,2095,2096,2086,2087,7080,11211,${MYSSH}\"';
-	NPO_OUT='TCP_OUT = \"20,21,22,25,53,80,110,113,443,465,587,993,995,2789,27635,1443,9085,9086,2077,2078,2082,2083,2095,2096,2086,2087,7080,11211,${MYSSH}\"'
-elif [ -d "/usr/local/directadmin" ]; then
-	NPO_IN='TCP_IN = \"20,21,22,25,53,80,110,143,443,465,587,993,995,2222,2789,27635,1443,9085,9086,7080,11211,${MYSSH}\"';
-	NPO_OUT='TCP_OUT = \"20,21,22,25,53,80,110,113,443,465,587,993,995,2222,2789,27635,1443,9085,9086,7080,11211,${MYSSH}\"'
-elif [ -d "/usr/local/cwp" ]; then
-	NPO_IN='TCP_IN = \"20,21,22,25,53,80,110,143,443,465,587,993,995,2789,27635,1443,9085,9086,7080,11211,2030,2031,${MYSSH}\"';
-	NPO_OUT='TCP_OUT = \"20,21,22,25,53,80,110,113,443,465,587,993,995,2789,27635,1443,9085,9086,7080,11211,2030,2031,${MYSSH}\"'
-fi
-
-function  do_config()
-{
-	clear;
-	echo -e "$GREEN [NOTICE] CSF configuraing... $RESET";
-	
-	rpm -qa | grep ipset;
-	if [ $? -ne 0 ]; then
-		yum install ipset -y >/dev/null 2>&1;
-	fi
-	csf -x >/dev/null 2>&1 ;
-	echo "$GREEN[NOTICE] Configure Process is runnig...$RESET";
-	perl -pi -e 's/TESTING = "1"/TESTING = "0"/g' "${CONFIG}"
-	perl -pi -e 's/RESTRICT_SYSLOG = "0"/RESTRICT_SYSLOG = "3"/g' "${CONFIG}"
-	perl -pi -e 's/SYSLOG_CHECK = "0"/SYSLOG_CHECK = "300"/g' "${CONFIG}"
-	perl -pi -e 's/DENY_TEMP_IP_LIMIT = "100"/DENY_TEMP_IP_LIMIT = "200"/g' "${CONFIG}"
-	perl -pi -e 's/LF_IPSET = "0"/LF_IPSET = "1"/g' "${CONFIG}"
-	perl -pi -e 's/LF_SELECT = "0"/LF_SELECT = "1"/g' "${CONFIG}"
-	perl -pi -e 's/CT_LIMIT = "0"/CT_LIMIT = "200"/g' "${CONFIG}"
-	
-	OE=$(cat "${CONFIG}" | grep "LF_ALERT_TO = " )
-	NE="LF_ALERT_TO = \"${TO}\""
-	perl -pi  -e "s/${OE}/${NE}/g"  "${CONFIG}"
-	
-	OF=$(cat "${CONFIG}" | grep "LF_ALERT_FROM = " )
-	NF="LF_ALERT_FROM = \"${FROM}\""
-	perl -pi  -e "s/${OF}/${NF}/g"  "${CONFIG}"
-	
-	PO_IN=$(cat "${CONFIG}" | grep "TCP_IN = " )
-	
-	perl -pi  -e "s/${PO_IN}/${NPO_IN}/g"  "${CONFIG}"
-
-	PO_OUT=$(cat "${CONFIG}" | grep "TCP_OUT =" )
-	perl -pi  -e "s/${PO_OUT}/${NPO_OUT}/g"  "${CONFIG}"
-	
-	USERMEM=$(cat "${CONFIG}" | grep "PT_USERMEM = ")
-	perl -pi  -e "s/${USERMEM}/PT_USERMEM = \"0\"/g"  "${CONFIG}"
-	
-	perl -pi -e 's/PT_LIMIT = "60"/PT_LIMIT = "0"/g' "${CONFIG}"
-	perl -pi -e 's/PT_SKIP_HTTP = "0"/PT_SKIP_HTTP = "1"/g' "${CONFIG}"
-	perl -pi -e 's/LF_EMAIL_ALERT = "1"/LF_EMAIL_ALERT = "0"/g' "${CONFIG}"
-	perl -pi -e 's/LF_PERMBLOCK_ALERT = "1"/LF_PERMBLOCK_ALERT = "0"/g' "${CONFIG}"
-	
-	echo "exe:/usr/bin/memcached" >> "${IGNORE}"
-	echo "exe:/sbin/rpcbind" >> "${IGNORE}"
-	
-	service rpc stop  >/dev/null 2>&1 ;
-	service xfs stop  >/dev/null 2>&1 ;
-	service portmap stop  >/dev/null 2>&1;
-	chkconfig cfx off  >/dev/null 2>&1 ;
-	service portreserve stop >/dev/null 2>&1 ;
-	chkconfig portreserve off >/dev/null 2>&1 ;
-
-	csf -e >/dev/null 2>&1 ;
-	csf -r >/dev/null 2>&1 ; 
-	echo -e "$GREEN [OK] finish configure $RESET";
-	echo -e "$GREEN";
-	read -p "Press any key to continue..." -n1 -s ;
-	echo -e "$RESET";
-}
-function do_install()
-{
-	clear;
-	cd /tmp;
-	echo -e "$GREEN [NOTICE] CSF Instaling... $RESET";
-	wget https://download.configserver.com/csf.tgz >/dev/null 2>&1 ;
-	tar -xzf csf.tgz >/dev/null 2>&1 ;
-	cd csf ;
-	sh install.sh >/dev/null 2>&1 ;
-	cd .. ;
-	rm -f csf.tgz ;
-	rm -rf csf/ ;
-	echo -e "$GREEN [OK] CSF installed! $RESET";
-	echo -e "$GREEN";
-	read -p "Press any key to continue... " -n1 -s ;
-	echo -e "$RESET";
-}
-
-
-function do_extra() {
-	clear;
-	echo -e "$GREEN [Notice] CSF Extra packages installing...! $RESET";
-	
-	rpm -qa | grep perl-libwww-perl;
-	if [ $? -ne 0 ]; then
-		yum install perl-libwww-perl -y  >/dev/null 2>&1;
-	fi
-	
-	rpm -qa | grep perl-Crypt-SSLeay;
-	if [ $? -ne 0 ]; then
-		yum install perl-Crypt-SSLeay -y  >/dev/null 2>&1 ;
-	fi
-	rpm -qa | grep perl-Time-HiRes;
-	if [ $? -ne 0 ]; then
-		yum install  perl-Time-HiRes -y  >/dev/null 2>&1;
-	fi
-	yum install 'perl (IO::Socket:SSL)'  >/dev/null 2>&1;
-	echo -e "$GREEN [OK] Extra packages installed! $RESET";
-	echo -e "$GREEN";
-	read -p "Press any key to continue... " -n1 -s ;
-	echo -e "$RESET";echo -e "$RESET";
-}
-
-################################################################################
-while true
-do
-
-### display main menu ###
-dialog --clear  --help-button --backtitle "Asanwebhost Company" \
---title "[ M A I N - M E N U ]" \
---menu "You can use the UP/DOWN arrow keys, the first \n\
-letter of the choice as a hot key, or the \n\
-number keys 1-9 to choose an option.\n\
-Choose the TASK" 15 60 6 \
-Install "install CSF" \
-config "only config csf" \
-Extra "install extra package" \
-Full "install & configure all needed options" \
-Exit "Exit to the shell" 2>"${INPUT}"
-
-menuitem=$(<"${INPUT}")
-
-
-# make decsion 
-case $menuitem in
-	Install) do_install;;
-	config) do_config;;
-	Extra) do_extra;;
-	Full) do_install;do_config;do_extra;;
-	Exit) clear;echo "Bye"; break;;
-esac
-
-done
+# clear temporary files
+run_command('rm -rf /tmp/csfinstall')
